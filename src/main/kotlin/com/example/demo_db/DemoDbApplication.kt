399d2fa7cd04ee0a1c9c7fb794c4ca566c3d18bc
ttatsato <t-satoh@alby.co.jp>
2019-11-10 22:31:38 +0900

    posgreと接続

---
+++ src/main/kotlin/com/example/demo_db/DemoDbApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo_db
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoDbApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoDbApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demo_db/DemoDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demo_db
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoDbApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
